3d15dfa750952d4f941344c5c4a6a3538c08715a
CKolean <esievanen@outlook.com>
2024-01-27 20:29:34 +0200

    Pelottava commit
    Mahdollisesti korruptoitunut menu beaware

--- Lost Control/menu/menu.gd
+++ Lost Control/menu/menu.gd
@@ -3,16 +3,27 @@ extends Node2D
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
+var story_finished = false
+var victory = true
+@onready var slide_timer: Timer = $slide_timer
+# ruma tapa saada lista slideistä
+@onready var storySlidesList = get_tree().get_nodes_in_group("story_slides_group")
+
 func _on_exit_btn_pressed():
 	print("exit")
 	get_tree().quit()
 
-
 func _on_start_btn_pressed():
-	print("start")
-	get_tree().change_scene_to_file("res://main.tscn")
-
+	print("listing",storySlidesList)
+	$main_menu_graphics/buttons/start_btn.set_visible(false)
+	$main_menu_graphics/buttons/next_btn.set_visible(true)
+	$story_slides/story1.set_visible(true)
+	slide_timer.start(5)
 
 func _on_next_btn_pressed():
 	print("next")
+
+
+func _on_slide_timer_timeout():
+	get_tree().change_scene_to_file("res://main.tscn")
 

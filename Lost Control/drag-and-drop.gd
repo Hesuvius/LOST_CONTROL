469c8166732797b3091034eb2df632cdde7f418d
Tatu Arvela <tatu.arvela@nitor.com>
2024-01-27 16:24:25 +0200

    käsi liikuttaa sitä

--- Lost Control/drag-and-drop.gd
+++ Lost Control/drag-and-drop.gd
@@ -1,23 +1,27 @@
 extends Sprite2D
 
+var touching = false
 var selected = false
 
 
-func _on_area_2d_input_event(viewport, event, shape_idx):
+func _on_area_2d_input_event(_viewport, _event, _shape_idx):
 	pass
 
 
 func _input(event):
-	if selected && event is InputEventMouseMotion:
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if selected && event is InputEventMouseMotion:
 			var relative_movement = event.relative
 			position.x = position.x + relative_movement.x
 			position.y = position.y + relative_movement.y
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				selected = event.pressed
 
 
-func _on_area_2d_body_entered(body):
-	selected = true
+func _on_area_2d_body_entered(_body):
+	touching = true
 
 
-func _on_area_2d_body_exited(body):
-	selected = false
+func _on_area_2d_body_exited(_body):
+	touching = false

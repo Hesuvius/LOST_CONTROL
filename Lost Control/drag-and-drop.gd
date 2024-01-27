f88bf379947aeae53e46f47fba04ee8eda596293
Tatu Arvela <tatu.arvela@nitor.com>
2024-01-27 16:41:37 +0200

    fiksaillaan skribuloita

--- Lost Control/drag-and-drop.gd
+++ Lost Control/drag-and-drop.gd
@@ -4,17 +4,13 @@ var touching = false
 var selected = false
 
 
-func _on_area_2d_input_event(_viewport, _event, _shape_idx):
-	pass
-
-
 func _input(event):
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if selected && event is InputEventMouseMotion:
 			var relative_movement = event.relative
 			position.x = position.x + relative_movement.x
 			position.y = position.y + relative_movement.y
-		if event is InputEventMouseButton:
+		if touching && event is InputEventMouseButton:
 			if event.button_index == MOUSE_BUTTON_LEFT:
 				selected = event.pressed
 
@@ -25,3 +21,4 @@ func _on_area_2d_body_entered(_body):
 
 func _on_area_2d_body_exited(_body):
 	touching = false
+	selected = false

31c451446e7b27c27107a739249ac60cd5320c8a
CKolean <esievanen@outlook.com>
2024-01-27 15:09:14 +0200

    Adding Basic Menu with Start and exit
    Adding basic menu that can be used to start the game (load main scene)

---
+++ Lost Control/Menu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+
+func _on_exit_btn_pressed():
+	print("exit")
+	get_tree().quit()
+
+func _on_start_btn_pressed():
+	print("start")
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_next_btn_pressed():
+	print("next")
